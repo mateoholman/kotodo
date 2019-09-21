2fe4b12ce8086e3eb9a4d8c800a43d10bb7f7e80
Matthew Holman <mateoholman@gmail.com>
2019-09-21 18:49:03 -0500

    Basic todo functionality

--- todo.js
+++ todo.js
@@ -1,6 +1,6 @@
 const toDoListModel = function(items) {
   this.items = ko.observableArray(items);
-  this.newItem = ko.obervable("");
+  this.newItem = ko.observable("");
   this.addItem = function addNewTodo(item) {
     if (this.newItem) {
       this.items.push(this.newItem());
@@ -9,4 +9,4 @@ const toDoListModel = function(items) {
   }.bind(this); //Ensure 'this' is always the view model
 };
 
-ko.applyBindings(new toDoListModel([]));
+ko.applyBindings(new toDoListModel(['one', 'two', 'three']));
